8993933b59d08ea440490d32c8185eeae00f6df4
Gabriel <Gabrieltloubiel007@gmail.com>
2026-08-12 21:24:05 -0300

    Update Supabase anon key in supabase.js

--- JS/supabase.js
+++ JS/supabase.js
@@ -1,6 +1,6 @@
 // JS/supabase.js
 const SUPABASE_URL = 'https://sfmcwsodsyjdxxqmxeen.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmbWN3c29kc3lqZHh4cW14ZWVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU5NjM5MzcsImV4cCI6MjEwMTUzOTkzN30';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmbWN3c29kc3lqZHh4cW14ZWVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU5NjM5MzcsImV4cCI6MjEwMTUzOTkzN30.m_p14VSR0JNb24TumsNxh3kYpNSS_mEMr_p4W-XVAzk';
 
 // Declara a variável globalmente
 window._supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
