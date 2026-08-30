204bc87e5cc45f4d4d9602792e88f9c8cb241abe
Gabriel <Gabrieltloubiel007@gmail.com>
2026-08-30 02:42:17 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- JS/game.js
+++ JS/game.js
@@ -98,7 +98,7 @@ const RANKS = [
   { name: "SSS", minScore: 22000, color: "#f44336" }
 ];
 
-const ADMIN_PASSWORD_HASH = "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"; // Exemplo SHA-256
+// Exemplo SHA-256
 
 let text = "Typing Hero é um jogo de digitação dinâmico focado em precisão e agilidade.";
 let gameActive = false;
